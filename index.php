569136ff3931752a5f0b2d479b8b8f0fd4fda179
isabelgling <igl3@cornell.edu>
2017-04-23 18:22:56 -0400

    Push to master

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php header('Location: pages/index.php')?>
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Cornell in Vietnam | Home</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+
+<body>
+
+<?php
+	include("includes/nav.php");
+?>
+
+</body>
+
+</html>
+
