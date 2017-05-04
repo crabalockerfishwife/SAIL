9a770ddc08d6d6f04210eda00e5dbb12bc11e2cd
isabelgling <igl3@cornell.edu>
2017-05-03 22:07:07 -0400

    Added application page content

--- pages/application.php
+++ pages/application.php
@@ -21,16 +21,20 @@
 	?>
 	<div class="page-content">
 		<div class="page-heading">Application</div>
-		<div class="page-heading-2">A sub-heading</div>
-			<button class="accordion">Expand this</button>
+			<button class="accordion">Requirements</button>
 			<div class="panel">
-  				<p>Some content</p>
+  				<ul class>
+  					<li>- Upper-level students only.</li>
+  					<li>- Minimum GPA of 3.0 and good standing with the University (no pending Judicial Administrative violations). This GPA must be maintained over the program.</li>
+  					<li>- Permission code for ASIAN 3360 will be issued after your acceptance into the program.</li>
+  					<li>- All accepted students must enroll in VIET 1100 (1 credit, fall only)</li>
+  				</ul>	
 			</div>
-			<button class="accordion">Expand this</button>
+			<button class="accordion">Qualifications</button>
 			<div class="panel">
-  				<p>Some content</p>
+  				<p>The ideal candidate will demonstrate cross-cultural skills, flexibility, adaptability and maturity. Candidates should have demonstrated interests in Vietnam/Southeast Asia and issues on climate change, international development, and environmental studies. Candidates should exhibit excellent written and oral communication skills and have experience working as part of a team. Selected students must be willing to engage in service learning activities in local communities in Vietnam.</p>
 			</div>
-		<div class="page-heading-2">Another sub-heading</div>
+		<div class="panel">Please note that this course is NOT an opportunity for a holiday. It should be treated as an opportunity for intense international experience with serious academic work and intercultural exchange. The field component will require a high level of cooperation and collegiality with traveling companions.</div>
 	</div>
 
 	<div class="footer-bar"><br>© SAIL 2017</div>
