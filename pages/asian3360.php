8f2dc90f9468f06d7c411eec7711c26b44ecc150
Alexa Paley <alexapaley@dhcp-gs-6712.eduroam.cornell.edu>
2017-05-08 13:27:51 -0400

    added information to asian3360 page

--- pages/asian3360.php
+++ pages/asian3360.php
@@ -10,27 +10,28 @@
 		include("../includes/header.php");
    		include("../includes/nav_pages.php");
 
-	//    require_once ("../includes/config.php");
-	//    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-	//    
-	//    if($mysqli->connect_error) {
-	//        die("Connection failed: " . $mysqli->connect_error);
-	//    } else {
-	//        echo NULL;
-	//    }
 	?>
 	<div class="page-content">
 		<div class="page-heading">ASIAN 3360</div>
-		<div class="page-heading-2">A sub-heading</div>
-			<button class="accordion">Expand this</button>
-			<div class="panel">
-  				<p>Some content</p>
-			</div>
-			<button class="accordion">Expand this</button>
-			<div class="panel">
-  				<p>Some content</p>
-			</div>
-		<div class="page-heading-2">Another sub-heading</div>
+        <div class="page-heading-4">
+		<p>We live in an interconnected and interdependent world and with our changing climate this connectivity is becoming even more apparent. We are all in this together and will only solve this grand challenge by developing new global partnerships. This course was created as a way to help us all become more informed global citizens through firsthand experience of impacts due to climate change while gaining meaningful international engagements with the people in the Mekong Delta, Vietnam.</p>
+ 
+        <p>The course is intended to provide you an introduction to Vietnam through the lens of climate change and its implications, not only globally but also how it has impacted the Mekong Delta region of Vietnam, one of the most at risk areas in the world in particular. The combination of classroom lectures, historical and cultural tours, and service learning experience while in the country will challenge you to grasp the enormous challenges posed by climate change while examining and considering possible solutions. From what you learn and experience during this course, we hope you will be a better-informed global citizen and able to share the climate change story beyond your education at Cornell.</p>
+        </div>
+        
+        <div class="page-heading"> By the end of this course, you will:</div>
+        <div class="page-heading-4">
+  				<ul class>
+  					<li>• Respect and appreciate the culture, history and resilience of Vietnam and its people.</li>
+ 					<li>• Comprehend what climate change means to an agrarian country like Vietnam and implications for the global food system.</li>
+ 					<li>• Formulate solutions to the challenges posed by climate change to the Mekong Delta of Vietnam and beyond.</li>
+ 					<li>• Assess what was gained from the course and integrate this into your personal and professional life-path.</li>
+                    <li>• Realize the importance of sharing the climate change story with others.</li>
+                    <li>• Gain practical and meaningful international experience.</li>
+                    <li>Enhance your sense of worldview, civic engagement and social responsibility.</li>
+ 				</ul>
+        </div>
+
 	</div>
 
 	<div class="footer-bar"><br>© SAIL 2017</div>
