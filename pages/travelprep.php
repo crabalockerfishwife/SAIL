296fbcd4896e4bc56fdaa1faf97b326788bb65eb
Alexa Paley <alexapaley@Alexas-MacBook-Pro.local>
2017-05-09 17:35:31 -0400

    added information to travel prep page

--- pages/travelprep.php
+++ pages/travelprep.php
@@ -9,29 +9,88 @@
 	<?php
 		include("../includes/header.php");
    		include("../includes/nav_pages.php");
-
-	//    require_once ("../includes/config.php");
-	//    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-	//    
-	//    if($mysqli->connect_error) {
-	//        die("Connection failed: " . $mysqli->connect_error);
-	//    } else {
-	//        echo NULL;
-	//    }
 	?>
 	
 	<div class="page-content">
 		<div class="page-heading">Travel Preparation</div>
-		<div class="page-heading-2">A sub-heading</div>
-			<button class="accordion">Expand this</button>
+        <div class="page-heading-4">In the Mekong Delta, January is the dry season, with temperatures ranging from 72 to 90 degree Fahrenheit. As such, read below for suggestions on packing light for your 3 week stay.</div>
+			
+            <button class="accordion">Apparel and Outerwear</button>
 			<div class="panel">
-  				<p>Some content</p>
+  				<ul class>
+                    <h3>Apparel</h3>
+                        <li>• Tops: 4-5 pieces (T-shirts/blouses - 1 nice, 1 not so nice, 1 long sleeved)</li><br>
+                        <li>• Bottoms: 4-5 pieces (2 long pants, 2-3 shorts/skirts/dresses - presentable length)</li><br>
+                        <li>• Shoes: 1 sport sandals, 1 nice sandals/shoes</li><br>
+                        <li>• Note: sport sandals will be necessary for walking/hiking/beach - nothing too heavy</li><br>
+                        <li>• Optional: flip-flops for the bathroom/beach.</li><br>
+                        <li>• Other: 4 undergarments, 1 sleepwear, 2 socks, (swimwear and goggles optional)</li><br>
+                        <li>• Note: No jeans or heavy jeans type stuff (if you must, to be worn on the plane)</li>
+                    <h3>Apparel Accessories</h3>
+                        <li>• Sunglasses, regular glasses - if applicable</li><br>
+                        <li>• Scarves/sarongs</li><br>
+                        <li>• Hats, belts</li><br>
+                        <li>• Watch/alarm clock</li><br>
+                        <li>• A small towel (available in HCMC)</li><br>
+                        <li>• Optional: a sleeping sack (available in HCMC)</li><br>
+                    <h3>Outerwear</h3>
+                        <li>• 1 light sweater </li><br>
+                        <li>• 1 rain/wind resistant jacket </li><br>
+                </ul>
 			</div>
-			<button class="accordion">Expand this</button>
+        
+			<button class="accordion">Toiletries</button>
 			<div class="panel">
-  				<p>Some content</p>
+                <ul class>
+                    <li>• Essentials: Toothbrush, toothpaste, shampoo, soap etc. in travel sizes</li><br>
+                    <li>• Personal care items: cosmetics, lotions</li><br>
+                    <li>• Other: Eye care, contact solution, earplugs, eye masks</li><br>
+                    <li>• Note: all liquids in Ziploc bags</li><br>
+                    <li>• Extra Ziploc bags!</li><br>
+                </ul>
 			</div>
-		<div class="page-heading-2">Another sub-heading</div>
+        
+            <button class="accordion">Necessities</button>
+			<div class="panel">
+  				<ul class>
+                    <li>• Passport &amp; and a photocopy</li><br>
+                    <li>• Second wallet</li><br>
+                    <li>• Credit card (record/photo number)</li><br>
+                    <li>• Cash ($60-100 required at the airport)</li><br>
+                    <li>• Cornell student ID Card</li><br>
+                </ul>
+			</div>
+        
+            <button class="accordion">Miscellaneous</button>
+			<div class="panel">
+  				<ul class>
+                    <h3>Electronics</h3>
+                        <li>• Phone (unlocked)</li><br>
+                        <li>• Computer/iPad</li><br>
+                        <li>• Camera</li><br>
+                        <li>• Chargers/USB cables/headphones</li><br>
+                        <li>• Adapters/converters (from 3 prong to 2 prong - a must for your computer)</li>
+                    <h3>Medical Supplies</h3>
+                        <li>• Mosquito repellent</li><br>
+                        <li>• Sun block and after-sun block</li><br>
+                        <li>• Basic first aid (bandages, ointment, etc.)</li><br>
+                        <li>• Basic medicines (aspirin/Ibuprofen, vitamin C, cough/stomach medicines)</li>
+                    <h3>School Materials</h3>
+                        <li>• Notebooks (available in HCMC for less, may not be a name brand)</li><br>
+                        <li>• Journals &amp; pens (available in HCMC)</li><br>
+                        <li>• Reading materials</li>
+                    <h3>Luggage</h3>
+                        <li>• Preferably backpacks or carry-ons (check-in bags should be reasonable in size, 24”)</li><br>
+                        <li>• A light, small backpack (day pack/tote) to travel during the day; should have zippers</li><br>
+                        <li>• Small padlocks</li><br>
+                        <li>• Optional: 1 expandable/foldable bag/tote for extra souvenirs (available in HCMC)</li>
+                    <h3>Other</h3>
+                        <li>• Gifts for hosts</li><br>
+                        <li>• Games: playing cards, hacky sacks, frisbees etc.</li><br>
+                        <li>• Optional: small flashlight</li>
+                </ul>
+			</div>
+
 	</div>
 
 	<div class="footer-bar"><br>© SAIL 2017</div>
